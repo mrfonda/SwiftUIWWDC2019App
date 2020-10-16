75761d67d53b067d3269c29f391783d3584f8866
lilia <lilial@top4.com>
2020-10-16 13:20:01 +0300

    Initial Commit

---
+++ SwiftUIWWDC2019/SwiftUIWWDC2019App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWWDC2019App.swift
+//  SwiftUIWWDC2019
+//
+//  Created by Yoav Dror on 16/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWWDC2019App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
